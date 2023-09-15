3eecc468fa294c60d7396f59161b012ecd5ed46e
yotamaruyama <maruyama.youta2002@gmail.com>
2023-09-15 10:03:48 +0900

    コンテナが起動した

--- T2/settings.py
+++ T2/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'js'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
